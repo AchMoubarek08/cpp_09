9b8f9d660c05e75a2984af033c32ea70d885a54c
Achraf Moubarek <amoubare@e2r10p11.1337.ma>
2023-04-16 01:35:44 +0000

    parse ex00done

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -29,14 +29,41 @@ BitcoinExchange::BitcoinExchange(std::string filename)
     }
     infile.close();
 }
+
+int check_values(std::string &date, float &val)
+{
+    int year;
+    int mounth;
+    int day;
+    try
+    {
+    year = std::stoi(date.substr(0, 4));
+    mounth = std::stoi(date.substr(6, 7));
+    day = std::stoi(date.substr(8, 9));
+    }
+    catch(std::exception &e)
+    {}
+    if (date.size() != 10 || date[4] != '-' || date[7] != '-')
+        return (1);
+    if(val < 0 || val > 1000)
+        return(1);
+    if(year < 2009 || year > 2022 || mounth > 12 || mounth < 0 || day < 1 ||  day > 31)
+        return(1);
+    return(0);
+}
 int check_line(std::string line, std::string &date, char &sep, float &val)
 {
+    std::string check;
     std::istringstream iss(line);
-    if (!(iss >> date >> sep >> val) || sep != '|') 
+    if (!(iss >> date >> sep >> val) || sep != '|')
     {
         std::cerr << "Error: bad input => " << line << std::endl;
         return(1);
     }
+    if((iss >> check))
+        std::cerr << "Error: bad input => " << line << std::endl;
+    if(check_values(date, val))
+        std::cerr << "Error: bad input => " << line << std::endl;
     return(0);
 }
 void BitcoinExchange::exchange(std::string file)
@@ -48,11 +75,18 @@ void BitcoinExchange::exchange(std::string file)
         return;
     }
     std::string line;
+    std::getline(infile, line);
     while(std::getline(infile, line))
     {
         std::string date;
         char sep;
         float val;
+        try
+        {
+            line.replace(line.find(','), 1, ".");
+        }
+        catch(std::exception &e)
+        {}
         check_line(line, date, sep, val);
     }
 }
